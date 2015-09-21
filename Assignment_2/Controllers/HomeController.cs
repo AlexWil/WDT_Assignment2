bc6338bf6e8eaa56aabbeac2a4a6b22592dbdfdc
knave0 <knave0@gmail.com>
2015-09-21 18:57:03 +1000

    Added Part 1 pieces

---
+++ Assignment_2/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignment_2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Cinemas()
+        {
+            ViewBag.Message = "Cinemas List";
+
+            return View();
+        }
+        public ActionResult Movies()
+        {
+            ViewBag.Message = "Movies List";
+
+            return View();
+        }
+        public ActionResult Events()
+        {
+            ViewBag.Message = "Events List";
+
+            return View();
+        }
+        public ActionResult Movie_Sessions()
+        {
+            ViewBag.Message = "Movies Session Times";
+
+            return View();
+        }
+
+    }
+}
